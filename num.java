47b036e00c34dca5c56b17542e1713ccb0de6157
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-30 22:58:16 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int m=s.nextInt();
+	int h=5,d;
+	for(int i=1;i<=h;i++)
+	{
+		d=m*i;
+		System.out.print(d+" ");
+	}// your code goes here
+	}
+}
